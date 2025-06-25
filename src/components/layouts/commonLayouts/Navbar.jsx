63551fe2976039f1c73a1d6168eaf4b08fbbee61
afsanaj-dev <afsana2884@gmail.com>
2025-06-25 20:25:34 +0600

    navbar modify

--- src/components/layouts/commonLayouts/Navbar.jsx
+++ src/components/layouts/commonLayouts/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import logo from "../../../../public/logo.png"
 import { Link } from 'react-router-dom'
 import { FaBars } from "react-icons/fa";
+import { BsThreeDots } from "react-icons/bs";
 
 const Navbar = () => {
   return (
@@ -9,21 +10,33 @@ const Navbar = () => {
       <div>
         <img src={logo} alt="logo" />
       </div>
-      <div className='flex justify-between'>
-        <ul className='flex gap-12 items-center'>
+      <div className="flex justify-between">
+        <ul className="flex gap-12 items-center">
           <li>
             <Link className="text-white">
               <FaBars />
             </Link>
           </li>
+
           <li>
-            <Link className="text-white font-poppins font-bold text-sm">Careers</Link>
+            <Link className="text-white font-poppins font-bold text-sm">
+              Careers
+            </Link>
           </li>
           <li>
-            <Link className="text-white font-poppins font-bold text-sm">Store</Link>
+            <Link className="text-white font-poppins font-bold text-sm">
+              Store
+            </Link>
           </li>
           <li>
-            <Link className="text-white font-poppins font-bold text-sm">FAQ</Link>
+            <Link className="text-white font-poppins font-bold text-sm">
+              FAQ
+            </Link>
+          </li>
+          <li>
+            <Link className="text-white">
+              <BsThreeDots />
+            </Link>
           </li>
         </ul>
       </div>
